8d7d1ccdd19ee7faf5c9da72ea0ffbab86ea94f6
Munchquim <qpinedasotes@gmail.com>
2023-06-22 09:31:11 +0200

    terminado carta

--- typescript/ejercicios/Ejercicio carta/main.js
+++ typescript/ejercicios/Ejercicio carta/main.js
@@ -1,63 +1,20 @@
 const rev = document.getElementById("reverso");
-const anv = document.getElementById("anverso");
-const max_ancho = 300;
-const vel = 1000;
+let interruptor = false;
 const int = document.getElementById("interactuable");
 int.addEventListener("mouseenter", hover);
 int.addEventListener("mouseleave", leave);
 //funciones//
-//funcion cara
 function hover() {
     rev.style.width = "0px";
-    anv.style.width = "0px";
-    let sec = 1;
-    let timer = setInterval(function () {
-        sec--;
-        if (sec == 0) {
-            clearInterval(timer);
-            rev.style.display = "none";
-            anv.style.display = "block"
-            hover_2();
-        }
-
-    }, 500);
-}
-
-
-function hover_2() {
-    let sec = 1;
-    let timer = setInterval(function () {
-        sec--;
-        if (sec == 0) {
-            clearInterval(timer);
-            anv.style.width = "300px";
-        }
-
-    }, 100);
-
 }
 function leave() {
-    anv.style.width = "0px";
-    let sec = 1;
-    let timer = setInterval(function () {
-        sec--;
-        if (sec == 0) {
-            clearInterval(timer);
-            anv.style.display = "none";
-            rev.style.display = "block"
-            leave_2();
-        }
-
-    }, 500);
-}
-function leave_2() {
-    let sec = 1;
-    let timer = setInterval(function () {
-        sec--;
-        if (sec == 0) {
-            clearInterval(timer);
-            rev.style.width = "300px";
-        }
-
-    }, 100);
+    rev.style.width = "300px";
+    if (interruptor == false) {
+        rev.src = "./img/card-back.png";
+        interruptor = true;
+    }
+    else {
+        rev.src = "./img/card-front.png";
+        interruptor = false;
+    }
 }
